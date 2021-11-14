cdb3b7c818969590a84e0ea674d8b84507a07056
Marc Hofmann <mhofmann713@Gmail.com>
2021-11-14 02:38:14 +0100

    cock & balls

--- Engine/include/Engine/include.h
+++ Engine/include/Engine/include.h
@@ -1,6 +1,7 @@
 #ifndef ENGINE_ENGINE_INCLUDE_H
 #define ENGINE_ENGINE_INCLUDE_H
 
+#include "pch.h"
 #include "misc.h"
 
 #endif // !ENGINE_ENGINE_INCLUDE_H
--- EngineTest/src/main.cpp
+++ EngineTest/src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 #include <Engine/include.h>
 
 int main() {
     std::cout << "Hello World: ";
     Engine::PrintMessage();
     std::cout << "\n";
+
+    auto i = new int{ 8 };
+
+    glm::tvec3<int> vec;
+    
+    _CrtDumpMemoryLeaks();
+    return EXIT_SUCCESS;
 }
